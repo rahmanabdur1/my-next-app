c2f021b96ea4747e44da812457d4675f04b1d1d3
rahmanabdur1 <rahmanabdurr65@gmail.com>
2024-10-20 00:34:15 +0300

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import BgTalk from "@/Components/BgTalk";
+import Proceed from "@/Components/Proceed";
+import Services from "@/Components/Services";
+
+
+const Home = () => {
+  return (
+    <div>
+    <Services/>
+    <Proceed/>
+    <BgTalk/>
+    </div>
+  );
+};
+
+export default Home;
